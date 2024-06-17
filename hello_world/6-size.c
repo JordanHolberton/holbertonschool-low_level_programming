ea2f03dedd3f321f53b40d94fdade5b6bcb349a8
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-17 13:38:52 +0200

    Add script for print the sizer of various types on the computer

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,14 +5,14 @@
  * Return: Do nothing
  */
 int main (void){
-	int intType;
-	float floatType;
-	double doubleType;
-	char charType;
+	int a;
+	float b;
+	double c;
+	char d;
 
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of int: %zu bytes\n", sizeof(a));
+	printf("Size of float: %zu bytes\n", sizeof(b));
+	printf("Size of double: %zu bytes\n", sizeof(c));
+	printf("Size of char: %zu byte\n", sizeof(d));
 	return (0);
 }
