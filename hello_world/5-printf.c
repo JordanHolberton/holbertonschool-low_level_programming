1c152ce9a923e0d2ae3c90d26d96eb9baf2999d3
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-17 13:15:21 +0200

    Add script in C for print exactly with printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Print exactly
+ *
+ * Return: Do nothing
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
