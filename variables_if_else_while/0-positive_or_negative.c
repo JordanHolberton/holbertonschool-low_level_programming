53528715a94eafc35756d5857135dd4565f1ef5a
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-17 15:56:34 +0200

    Add script for program random variable

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n>0){
-		printf("is positive\n")
-			else if (n==0){
-				printf("is zero\n")
-					else (n<0){
-						printf("is negative\n")
-							return (0);
-					}
+		printf("is positive\n");
+	}
+	else if (n==0){
+		printf("is zero\n");
+	}
+	else (n<0){
+		printf("is negative\n");
+		return (0);
+	}
