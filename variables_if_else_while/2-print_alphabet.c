42f21ebada7865306dd4c3fba1a211c8d4efe640
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-18 11:09:58 +0200

    Add script for print alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in lower cas
+ *
+ * Return= is 0
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z' ; low++)
+		putchar(low);
+	putchar('\n');
+	return (0);
+}
