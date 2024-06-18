3cd941481fe598f7feee8cfb6aea6ff343371cf9
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-18 15:01:43 +0200

    Add script for print number 0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print number 0 --> 9
+ *
+ * Return: Value 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+
+}
