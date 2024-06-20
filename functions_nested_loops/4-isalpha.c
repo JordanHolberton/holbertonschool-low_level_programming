6d3622bf699acb0571baff71bad2dc8909e28dbf
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-20 13:58:56 +0200

    Add script for print 1 when char is in lowercase or uppercase ans 0 if not

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isalpha - check if char is in uppercase
- *  @c : return 1 if c is in lowercase or 0 if not
+ * _isalpha - check if char is in uppercase or lowercase
+ *  @c : return 1 if c is in uppercase/lowercase or 0 if not
  *  Return: Always 0
  */
 int _isalpha(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
