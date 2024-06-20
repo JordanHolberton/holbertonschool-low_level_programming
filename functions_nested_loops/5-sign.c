10f30f36651d24b9b0055d9e06794f5ce20a0530
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-20 14:49:26 +0200

    Add script for print number and define if it's negativ positiv or O

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Script for print number
+ * @n : return + if greater than 0, 0 if equal to 0 and - if lesser than 0
+ * Return: 1 if greater 0 if equal to 0 and -1 if lesser than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+			return (1);
+	}
+	else if (0 == 0)
+	{
+		_putchar('0');
+			return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
