df161f9fcec127752a3cd17a51b9c757c2808afc
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-20 13:30:10 +0200

    Add script for print when char is in lowercase of uppercase

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _is lower - check if char is in lowercase
+ * _islower - check if char is in lowercase
  * @c : return 1 if c is in lowercase or 0 if not
  * Return: Always 0
  */
