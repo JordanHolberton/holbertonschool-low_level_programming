a3ef1b4d6ef9999da9203cc339f501c0e5f6c007
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-21 13:57:32 +0200

    Add script for addition two int

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - add two integers and return the result
+ * @i: first int
+ * @j: second int
+ *
+ * Return: result of integer i + integer j
+ */
+int add(int i, int j)
+{
+	return (i + j);
+
+}
