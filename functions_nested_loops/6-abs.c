6357b109a8ba4b2928d557a6d38a12176097ca17
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-21 10:16:30 +0200

    Add script for int a absolute value of a number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Script for compute the absolute value of an integrer
+ * @n: integer to be computed
+ *
+ * Return: Absolute value negativ or positiv
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
