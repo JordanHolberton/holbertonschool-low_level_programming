c4e5478ffb82f22467f950a6ded9655c931d15f2
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-21 15:03:42 +0200

    Add script for print all number to 98 (before and after)

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print all number to 98
+ * @i print number under 98 to 98
+ * @j print number out of 98 to 98
+ * @n base number
+ *
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+
+			else if  (i == 98)
+				printf("%d\n", i);
+		}
+
+	}
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
+}
