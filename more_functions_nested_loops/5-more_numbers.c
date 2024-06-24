abfb4540a54214b5034bda348c488565b0c7b18d
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-24 11:46:33 +0200

    Add script for print number 1 to 14, 10 times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - Print number 0 to 14, 10 times
+ *
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for(j = 0; j <= 9; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i >= 10)
+				_putchar(i / 10 + '0');
+			_putchar(i% 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
