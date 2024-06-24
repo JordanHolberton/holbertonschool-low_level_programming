51136bbcfbc5c6c027679c3a16afe5d6bb6e44b0
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-24 11:49:59 +0200

    Fix betty coding style for task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,13 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for(j = 0; j <= 9; j++)
+	for (j = 0; j <= 9; j++)
 	{
 		for (i = 0; i < 15; i++)
 		{
 			if (i >= 10)
 				_putchar(i / 10 + '0');
-			_putchar(i% 10 + '0');
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
