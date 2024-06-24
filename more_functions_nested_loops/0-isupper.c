55164ab0d9b01fe9445f3b7493b396a9f3388719
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-24 09:46:50 +0200

    Add script for define if letter is in uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - Défini si la sortie est une lettre de l'alphabet en majuscule ou non
+ * Return: 0 si non majuscule et 1 si majuscule
+ */
+ 
+int _isupper(int c)
+
+	{
+		return(c >= 'A' && c <= 'Z');
+	}
