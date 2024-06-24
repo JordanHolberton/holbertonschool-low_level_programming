cbd004e5be8c7af34c8e912d59f2e467b8b8a23e
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-24 10:35:15 +0200

    Add script multiplies 2 numbers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplie 2 numbers
+ * @resultat: multiplication into a * b
+ * return: resultat
+ */
+
+int mul(int a, int b)
+
+{
+	int resultat;
+
+	resultat = a * b;
+
+	return(resultat);
+}
