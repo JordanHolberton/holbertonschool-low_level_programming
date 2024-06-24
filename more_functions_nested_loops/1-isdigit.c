2a5166c181455ca0a892bc2c40c9199e10ee609e
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-24 10:04:23 +0200

    Add script for know if it's a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isdigit - Check if it's a digit
+ * @c: main function
+ * Return: 1 if digit 0 if not digit
+ */
+
+int _isdigit(int c)
+
+{
+	return (c >= '0' && c <= '9');
+}
