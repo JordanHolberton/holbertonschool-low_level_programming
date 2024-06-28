4c04021c55e0bac0e0ae24dc25bc982b8514a6c8
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-28 14:52:59 +0200

    function that capitalizes all words of a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -15,12 +15,12 @@ char *cap_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		{
-			for(j = 0; j < 12; j++)
+			for(j = 0; j < 13; j++)
 			{
 				if (s[i] == separators[j])
 				{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-					s[i + 1] -= 32;
+					if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+						s[i + 1] -= 32;
 				}
 			}
 		}
