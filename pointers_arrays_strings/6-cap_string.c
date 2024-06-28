8e11eb2766cd579b1030a25747d08dff47c7a4d4
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-28 15:03:25 +0200

    Fix betty codding style

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,18 +13,18 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-			if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-				s[i] -= 32;
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		{
+			for (j = 0; j < 13; j++)
 			{
-				for (j = 0; j < 13; j++)
+				if (s[i] == separators[j])
 				{
-					if (s[i] == separators[j])
-					{
-						if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-							s[i + 1] -= 32;
-					}
+					if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+						s[i + 1] -= 32;
 				}
 			}
+		}
 	}
 	return (s);
 }
