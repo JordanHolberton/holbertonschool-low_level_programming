a0f61e18bd301754d0cb1ae8904e2f1eab77ac6b
JordanHolberton <jordan.chatillon76@laposte.net>
2024-06-28 11:32:24 +0200

    Write a function that reverses the content of an array of integers

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reverse_array: function that reverses the content of an array of integers
+ * reverse_array - function that reverses the content of an array of integers
  * @a: an array of integers
  * @n: the nulber if elements to swap
  * Return:  0
