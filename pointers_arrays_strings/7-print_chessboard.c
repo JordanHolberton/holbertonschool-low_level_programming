80bcb810de606a9643b5c88d4a6124085ba4f928
JordanHolberton <jordan.chatillon76@laposte.net>
2024-07-01 13:32:32 +0200

    Write function that prints the chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  *print_chessboard - function that prints the chessboard
  *@a : two dimension array to print
  */
+
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
