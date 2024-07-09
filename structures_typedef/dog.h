84d442eae3dafccc905271f306dd021248463205
JordanHolberton <jordan.chatillon76@laposte.net>
2024-07-09 11:00:03 +0200

    add header file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - info aboud dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: no description
+ */
+
 struct dog
 {
 	char *name;
