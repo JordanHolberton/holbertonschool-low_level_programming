6e912fb67efd3b48fe7dd3d2853eda4906430aa1
JordanHolberton <jordan.chatillon76@laposte.net>
2024-08-01 10:33:42 +0200

    Add task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free dlistint_t list. MLP
+ * @head: beginning of the list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	while (head != NULL)
+	{
+		dlistint_t *temp = head->next;
+
+		free(head);
+		head = temp;
+	}
+}
